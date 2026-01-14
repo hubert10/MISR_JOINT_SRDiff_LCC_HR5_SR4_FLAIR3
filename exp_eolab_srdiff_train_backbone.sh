a54ed50ec61df349975c79abcbbf0e384883dbad
Hubert Kanyamahanga <kanyamahanga.hubert@aims-senegal.org>
2026-01-14 22:20:54 +0100

    first commit

--- exp_eolab_srdiff_train_backbone.sh
+++ exp_eolab_srdiff_train_backbone.sh
@@ -16,7 +16,7 @@
 export CONDA_ENVS_PATH=$HOME/.conda/envs
 DATA_DIR="/my_data/"
 export DATA_DIR
-source /home/eouser/flair_venv/bin activate
+source /home/eouser/flair_venv/bin/activate
 which python
 cd $HOME/exp_2026/MISR_JOINT_SRDiff_LCC_HR5_SR4_FLAIR3
 python trainer.py --config configs/diffsr_maxvit_ltae.yaml --config_file flair-config-server-eolab.yml --exp_name misr/srdiff_maxvit_ltae_ckpt --reset
